d4afc65327cca986ce01a76eb330185619e13cdf
majway27 <robert.p.may@gmail.com>
2022-05-30 05:27:53 -0600

    working relay

--- vite.config.js
+++ vite.config.js
@@ -3,27 +3,47 @@ import copy from 'rollup-plugin-copy'
 
 export default {
   build: {
-    chunkSizeWarningLimit: 5000
+    chunkSizeWarningLimit: 5000,
+    rollupOptions: {
+      external: [
+        "/sw.js",
+      ],
+    },
   },
   plugins: [
     VitePWA({
-      registerType: 'autoUpdate'
-      // base: './'
-      // manifest: {
-      //   // content of manifest
-      // },
-      // workbox: {
-      //   // workbox options for generateSW
-      //   swDest: './dist/sw.js',
-      //   globDirectory: './dist',
-      //   skipWaiting: true
-      // }
+      registerType: 'autoUpdate',
+      base: './',
+      manifest: {
+        "$schema": "https://json.schemastore.org/web-manifest-combined.json",
+        "name": "HackerWeb",
+        "short_name": "HackerWeb",
+        "start_url": ".",
+        "display": "standalone",
+        "background_color": "#fff",
+        "description": "A readable Hacker News app.",
+        "icons": [{
+          "src": "android-chrome-192x192.png",
+          "sizes": "192x192",
+          "type": "image/png"
+        }, {
+          "src": "favicon-32x32.png",
+          "sizes": "32x32",
+          "type": "image/png"
+        }],
+      },
+      workbox: {
+      // workbox options for generateSW
+        swDest: './dist/sw.js',
+        globDirectory: './dist',
+        skipWaiting: true
+      }
     }),
     copy({
       targets: [{ src: './static/*', dest: './dist' }],
       verbose: true,
       hook: 'writeBundle',
       copyOnce: true
-    })
+    }),
   ]
 }
